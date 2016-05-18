a4d169d3bc07de04a6c35a7dbeba956f889275e3
Baptiste Crépin <baptiste.crepin@ensimag.grenoble-inp.fr>
2016-05-18 13:59:30 +0200

    step0

---
+++ step0.R
@@ -0,0 +1,16 @@
+#taille de l'échantillon
+n = 20
+
+mean1 = 0
+mean2 = 0.1
+var = 1
+d = sqrt(var)
+
+x1 = rnorm(20,mean1,var)
+x2 = rnorm(20,mean2,var)
+e = (mean2 - mean1)/d
+
+alpha = 0.05
+u=1.65
+u2=3.1
+
