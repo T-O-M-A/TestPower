9eecf703a49dee75ceeb2ec76e33265a7e2cd4ed
Thomas Lair <thomas.lair@ensimag.grenoble-inp.fr>
2016-05-18 17:01:03 +0200

    MaJ Step1 -> Commentaires

--- step1.R
+++ step1.R
@@ -1,6 +1,15 @@
 # Objectif de l'étape 1:
 # Déterminer la taille d'effet du cas incongruent sur le cas congruent.
-# On part d'un nombre 
+# On part d'un nombre fixé d'observations, ici n = 1000, et on les duplique en utilisant du Bootstrap :
+#   Un grand nombre de fois (ici 3000), on va sélectionner 800 observations aléatoirement sur ces 1000,
+#   et on va calculer la taille d'effet observée sur ce tirage d'observation.
+#   Le alpha et la puissance (donc le beta) sont fixés, ce qui permet de calculer la taille d'effet nécessaire
+#   pour avoir une telle puissance étant donné ce alpha.
+#   Dans les faits, on tri les valeurs des 800 observations considérées, et la taille d'effet correspond à
+#   la différence entre la (1-alpha)ième valeurs et la (1-beta)ième.
+# Une fois le calcul effectué pour chaque itération du Bootstrap, on estime la taille d'effet désirée par 
+# la moyenne des tailles d'effets obtenues précédemments.
+
 
 tailledeffet<-function(mean, var, alpha, puissance)
 {
