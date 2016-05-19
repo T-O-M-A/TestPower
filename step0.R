b93df29c3270aa7ef173f5d468f8aa225b0352fb
BONJEAN Grégoire <gregoire.bonjean@ensimag.grenoble-inp.fr>
2016-05-19 09:45:02 +0200

    step0 commenté

--- step0.R
+++ step0.R
@@ -1,13 +1,42 @@
-tailledeffet<-function(mean1, mean2, var, alpha)
+# Objectif de l'étape 0: 
+# Estimer la puissance d'un test à 2 modalités
+
+# On fixe le risque de 1ère espèce, la taille de l'effet
+# connue a priori, et le nombre d'individus,
+# pour obtenir la puissance du test.
+
+
+# Calcul analytique de la puissance
+# pour 2 gaussiennes données, sans échantillon.
+# (permet de vérifier la cohérence des résultats)
+PuissanceAnalytique <- function(mean1, mean2, var, alpha)
 {
-  norm1 = rnorm(20, mean1, var)
-  norm2 = rnorm(20, mean2, var)
   taille_effet = (mean2 - mean1)/sqrt(var)
   
-  u = qnorm(1-alpha) + mean1
+  u = qnorm(1 - alpha) + mean1
   
   return(pnorm(mean2 - u))
 }
 
-p<-tailledeffet(0,2,1,0.05)
-p
+
+# Estimation de la puissance
+# à partir d'un échantillon de taille n 
+# de taille d'effet fixée 
+# (ie de moyennes et variances fixées)
+PuissanceEstimee <- function(mean1, mean2, var, alpha, n)
+{
+  norm1 = rnorm(n, mean1, var)
+  norm2 = rnorm(n, mean2, var)
+  
+  
+  u = qnorm(1 - alpha) + mean(norm1)
+  
+  return(pnorm(mean(norm2) - u))
+}
+
+# Tests : gaussiennes de moyenne 1 et 2 et de variance 1.
+# alpha = 0.05, n = 20.
+PA <- PuissanceAnalytique(1,2,1,0.05)
+PE <- PuissanceEstimee(1,2,1,0.05,20)
+PA
+PE
